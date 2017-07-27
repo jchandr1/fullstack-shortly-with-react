6e471d46a92820029b3e530aaa170785346cf75c
Hack Reactor Student <student@hackreactor.com>
2017-07-27 14:58:04 -0700

    setup stuff

---
+++ src/static/components/Expense.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default class Expense extends React.Component {
+  render() {
+    return (
+    );
+  }
+}
---
+++ src/static/components/ExpenseList.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class ExpenseList extends React.Component {
+  render(props) {
+    return (
+      <div className="expense-list">
+        expenseList.map(expense => {
+          <Expense key={expense.name}
+        })
+      </div>
+    );
+  }
+}
