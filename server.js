5a714f10123305f857c654fe43d359ff796961d5
Hack Reactor Student <student@hackreactor.com>
2017-07-28 12:22:08 -0700

    implementing post and get reqs

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const server = express();
 const parser = require('body-parser');
+const db = require('./db');
 
 server.use(express.static(__dirname + '/www'));
+server.use(parser.json());
 server.use(parser.urlencoded({ extended: true }));
 server.use((req, res, next) => {
   console.log(req.method, req.url);
   next();
 });
+
+server.post('/expense', (req, res) => {
+  console.log(req.body);
+  db.query('INSERT INTO expenses (')
+  res.redirect('/');
+})
 
 const app = server.listen(3000, function() {
   const host = app.address().address;
