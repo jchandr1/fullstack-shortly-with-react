69bd47c972f865cb5cac88573797058e0dee85ee
Hack Reactor Student <student@hackreactor.com>
2017-07-31 17:18:29 -0700

    added foreign key relationship

--- schema.sql
+++ schema.sql
@@ -9,22 +9,27 @@ DROP TABLE IF EXISTS funds;
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE expenses (
-  id INTEGER AUTO_INCREMENT,
+  id INT AUTO_INCREMENT,
   name VARCHAR(50),
-  cost INTEGER,
+  cost INT,
   currency VARCHAR(3),
-  USDVal INTEGER,
+  USDVal INT,
+  user_id INT,
+  INDEX user_ind (user_id),
+  FOREIGN KEY (user_id)
+    REFERENCES users(id)
+    ON DELETE CASCADE
   PRIMARY KEY (id)
 );
 
 CREATE TABLE fundsLeft (
-  id INTEGER AUTO_INCREMENT,
-  amt INTEGER,
+  id INT AUTO_INCREMENT,
+  amt INT,
   PRIMARY KEY (id)
 );
 
 CREATE TABLE users (
-  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  id INT AUTO_INCREMENT PRIMARY KEY,
   username VARCHAR(50),
   password VARCHAR(50)
 );
