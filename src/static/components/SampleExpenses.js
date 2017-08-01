89b7e26faff7194bb2770a0c851306392bd0d93f
Hack Reactor Student <student@hackreactor.com>
2017-08-01 11:10:52 -0700

    moving files

--- src/static/components/SampleExpenses.js
+++
@@ -1,3 +0,0 @@
-// Sample Expenses
-
-module.exports = [{ id: 1, name: 'food', USDVal: 100, cost: 100, currency: 'USD' }, { id: 2, name: 'parking', USDVal: 20, cost: 20, currency: 'USD' }, { id: 3, name: 'board', USDVal: 200, cost: 200, currency: 'USD' }]
